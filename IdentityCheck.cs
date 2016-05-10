b72754597f528f6045d0f5cfd737611260ff9c63
Gábor Bánhegyesi <gbanhegyesi@whitepages.com>
2016-05-10 14:08:18 +0200

    Initial

---
+++ IdentityCheck.cs
@@ -0,0 +1,57 @@
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Web;
+
+namespace Identity_Check_Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Build the URI
+            UriBuilder uri = new UriBuilder();
+            uri.Scheme = "https";
+            uri.Host = "proapi.whitepages.com";
+            uri.Path = "/3.1/identity_check.json";
+
+            var parameters = HttpUtility.ParseQueryString(string.Empty);
+            parameters.Add("api_key", Environment.GetEnvironmentVariable("ID_CHECK_API_KEY"));
+            parameters.Add("billing.name", "Drama Number");
+            parameters.Add("billing.phone", "6464806649");
+            parameters.Add("billing.address.street_line_1", "302 Gorham Ave");
+            parameters.Add("billing.address.city", "Ashland");
+            parameters.Add("billing.address.state_code", "MT");
+            parameters.Add("billing.address.postal_code", "59004");
+            parameters.Add("billing.assress.country_code", "US");
+            parameters.Add("shipping.name", "Drama Number");
+            parameters.Add("shipping.phone", "6464806649");
+            parameters.Add("shipping.address.street_line_1", "302 Gorham Ave");
+            parameters.Add("shipping.address.city", "Ashland");
+            parameters.Add("shipping.address.state_code", "MT");
+            parameters.Add("shipping.address.postal_code", "59004");
+            parameters.Add("shipping.assress.country_code", "US");
+            parameters.Add("email_address", "medjalloh1@yahoo.com");
+            parameters.Add("ip_address", "108.194.128.165");
+
+            uri.Query = parameters.ToString();
+
+            using(var httpClient = new HttpClient())
+            {
+                var rawJson = httpClient.GetStringAsync(uri.Uri).Result;
+            
+                // Parse JSON response
+                var jsonMap = JObject.Parse(rawJson);
+
+                var availableChecks = new [] {"billing_name_checks", "billing_phone_checks", "billing_address_checks",
+                                              "shipping_name_checks", "shipping_phone_checks", "shipping_address_checks",
+                                              "email_address_checks", "ip_address_checks"};
+                // Display results
+                foreach (var check in availableChecks) {
+                    Console.WriteLine(jsonMap[check].ToString());
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,33 @@
+# Whitepages Pro Code Examples in C# [![Build Status](https://travis-ci.org/whitepages/pro-examples-csharp.svg?branch=master)](https://travis-ci.org/whitepages/pro-examples-csharp)
 
+
+Use mcs and mono to compile and run C# sources on Mac OS X. Install mono and [nuget.sh].
+
+## Install
+
+Install [Newtonsoft.Json]:
+
+```shell
+nuget.sh install Newtonsoft.Json -Version 8.0.3
+```
+
+## Compile
+
+Compile the sources:
+
+```shell
+mcs -r:/path/to/Newtonsoft.Json.dll,System.Web.dll,System.Net.Http.dll IdentityCheck.cs
+mcs -r:/path/to/Newtonsoft.Json.dll,System.Web.dll,System.Net.Http.dll LeadVerify.cs
+```
+
+## Run
+
+Run the code with mono:
+
+```shell
+mono LeadVerify.exe
+mono IdentityCheck.exe
+```
+
+[nuget.sh]: https://gist.github.com/andypiper/2636885
+[Newtonsoft.Json]: http://www.newtonsoft.com/json
